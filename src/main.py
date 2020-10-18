9d61803a2ec335b19afe58415d0c769ec80b891b
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2020-10-18 23:56:13 +0300

    update main

--- src/main.py
+++ src/main.py
@@ -3,23 +3,46 @@ from modules.task1 import task1_task21
 from modules.task1 import task1_task31
 from modules.task1 import task1_task41
 
+from modules.task2 import task2_task11
+from modules.task2 import task2_task21
+from modules.task2 import task2_task31
+from modules.task2 import task2_task41
+
 def print_line():
     print("= = = = = - - - - - = = = = = - - - - - = = = = =")
 
 print("Task 1: 1.1")
 print_line()
-task1_task11(1,1,1,1)
-task1_task11(0,1,1,1)
-task1_task11(1,0,1,1)
+obj = task1_task11(1,1,1,1)
+obj = task1_task11(0,1,1,1)
+obj = task1_task11(1,0,1,1)
 
 print("Task 1: 2.1")
 print_line()
-task1_task21([-5, 6, -11, 3, '343', 10])
+obj = task1_task21([-5, 6, -11, 3, '343', 10])
 
 print("Task 1: 3.1")
 print_line()
-task1_task31([-5, 12.24, -11.4, 6, 30, 11.98, 10])
+obj = task1_task31([-5, 12.24, -11.4, 6, 30, 11.98, 10])
 
 print("Task 1: 4.1")
 print_line()
-task1_task41([-5, 12.24, -11.4, 6, 30, 11.98, 10])
+obj = task1_task41([-5, 12.24, -11.4, 6, 30, 11.98, 10])
+
+print("Task 2: 1.1")
+print_line()
+obj = task2_task11()
+
+print("Task 2: 2.1")
+print_line()
+obj = task2_task21(['aaaa', 'bbbbb', 'cccccc', 'ddd', 'eeeeeee', 'ff', 'gggggggggggg', 'hhhhhhhhhh'])
+
+print("Task 2: 3.1")
+print_line()
+obj = task2_task31()
+string = task2_task31.getResult(obj)
+
+print("Task 2: 4.1")
+print_line()
+obj = task2_task41(string)
+number = task2_task41.getResult(obj)
